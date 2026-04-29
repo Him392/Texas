bcf6ca0c29f0637af712115166a64801835977c7
Him392 <50799666+Him392@users.noreply.github.com>
2026-04-29 18:18:31 +0800

    RENAME

--- ESP32_H/src/led_pwm.h
+++
@@ -1,9 +0,0 @@
-#ifndef LED_PWM_H
-#define LED_PWM_H
-
-#include <Arduino.h>
-
-// 声明 ledcAnalogWrite 函数
-void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255);
-
-#endif // LED_PWM_H
